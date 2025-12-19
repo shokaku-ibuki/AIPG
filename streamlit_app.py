67d2984a29d017de685ac2b8bc358da70040ea8d
shokaku-ibuki <shokaku.ibuki@stu.kist.ac.jp>
2025-12-19 13:09:26 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,1 @@
-    fig = px.line(
-        df_season_drivers,
-        x="round",
-        y="points",
-        color="driverRef",
-        labels={"driverRef": "driver", "name": "Grand Prix"},
-        category_orders={"driverRef": list_order_points},
-    )
-    st.plotly_chart(fig, use_container_width=True)
 
